a2048f756a9b6f0585944dbfc3c36a032aba9dbc
Inderjit Gill <email@indy.io>
2013-08-28 10:59:42 +0100

    drawer ui

--- drone/src/main/java/io/indy/drone/MainActivity.java
+++ drone/src/main/java/io/indy/drone/MainActivity.java
@@ -16,33 +16,53 @@
 
 package io.indy.drone;
 
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import de.greenrobot.event.EventBus;
+import io.indy.drone.adapter.StrikeCursorAdapter;
 import io.indy.drone.adapter.TabsAdapter;
-import io.indy.drone.fragment.MapFragment;
-import io.indy.drone.fragment.NewsFragment;
-import io.indy.drone.fragment.StatsFragment;
+import io.indy.drone.event.UpdatedDatabaseEvent;
 import io.indy.drone.model.SQLDatabase;
 
 public class MainActivity extends ActionBarActivity {
 
     static private final boolean D = true;
     static private final String TAG = MainActivity.class.getSimpleName();
-
-    private SQLDatabase mDatabase;
-
     static void ifd(final String message) {
         if (D) Log.d(TAG, message);
     }
 
+    private String[] mDrawerTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+
+    private SQLDatabase mDatabase;
     TabsAdapter mTabsAdapter;
+
+
+    private StrikeCursorAdapter mStrikeCursorAdapter;
+    private ListView mListView;
+
 
     public SQLDatabase getDatabase() {
         return mDatabase;
@@ -51,23 +71,99 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get the view from activity_main.xml
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
 
         mDatabase = new SQLDatabase(getApplicationContext());
 
-        ifd("MainActivity hashcode " + System.identityHashCode(this));
+        setupNavigationDrawer();
 
-        setupTabs();
+        Cursor c = mDatabase.getStrikeCursor();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            mStrikeCursorAdapter = new StrikeCursorAdapter(this, c, 0);
+        } else {
+            mStrikeCursorAdapter = new StrikeCursorAdapter(this, c);
+        }
+
+        mListView = (ListView) findViewById(R.id.listViewNews);
+        mListView.setAdapter(mStrikeCursorAdapter);
+
+        if(mDatabase.hasData()) {
+            setSupportProgressBarIndeterminateVisibility(false);
+        } else {
+            setSupportProgressBarIndeterminateVisibility(true);
+        }
+
+    }
+
+    private void setupNavigationDrawer() {
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerTitles = getResources().getStringArray(R.array.locations_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mDrawerTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            ifd("selected " + position);
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mDrawerTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
     }
 
     @Override
     public void onDestroy() {
         ifd("onDestroy");
+
+        CursorAdapter ca = (CursorAdapter)(mListView.getAdapter());
+        ca.getCursor().close();
+
         mDatabase.closeDatabase();
         super.onDestroy();
     }
-
+/*
     private void setupTabs() {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -131,6 +227,12 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         ifd("clicked " + item);
 
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.action_settings:
                 ifd("clicked on settings");
@@ -139,5 +241,26 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        //menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
 
 }
